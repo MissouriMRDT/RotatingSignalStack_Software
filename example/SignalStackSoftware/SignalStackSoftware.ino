6eda2223f571c4f8ef04631a3349685ae34936cc
Grant Brinker <gsbfy2@umsystem.edu>
2023-03-02 01:13:23 -0600

    Made rotation fully functional

--- example/SignalStackSoftware/SignalStackSoftware.ino
+++ example/SignalStackSoftware/SignalStackSoftware.ino
@@ -14,7 +14,7 @@ void setup()
     // Communication setup
     Wire.begin();
     Serial.begin(9600);
-    //RoveComm.begin(RC_SIGNALSTACKBOARD_FOURTHOCTET, &TCPServer, RC_ROVECOMM_SIGNALSTACKBOARD_MAC);
+    RoveComm.begin(RC_SIGNALSTACKBOARD_FOURTHOCTET, &TCPServer, RC_ROVECOMM_SIGNALSTACKBOARD_MAC);
     delay(100);
     telemetry.begin(Telemetry, 1500000);
     Serial.println("Started: ");
@@ -33,9 +33,6 @@ void setup()
     // GPS pins
     Serial1.setRX(GPS_RX);
     Serial1.setTX(GPS_TX);
-
-    analogWrite(COIL1_FWD, 125);
-    analogWrite(COIL2_FWD, 125);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -43,15 +40,15 @@ void setup()
 void loop()
 {
     // Receive RoveComm commands
-    //packet = RoveComm.read();
-    //
-    //switch (packet.data_id)
-    //{
-    //    case RC_SIGNALSTACKBOARD_SIGNALSROTATE_DATA_ID:
-    //        stepNumber = ((int16_t*) packet.data)[0];
-    //        stackRotate(stepNumber);
-    //        break;
-    //}
+    packet = RoveComm.read();
+    
+    switch (packet.data_id)
+    {
+        case RC_SIGNALSTACKBOARD_SIGNALSROTATE_DATA_ID:
+            stepNumber = ((int16_t*) packet.data)[0];
+            stackRotate(stepNumber);
+            break;
+    }
 
     updateCompass();
     updateGPS();
@@ -97,21 +94,23 @@ void stackRotate(int16_t numSteps)  // Moves the motor a requested number of ste
 {   
     if (numSteps>0)
     {
-        for (uint8_t i=0; i<numSteps; i++)
+        for (uint16_t i=0; i<numSteps; i++)
         {
             motorStep(motorPWM);
             if (!(stepCount % 4)) stepCount = 1;    // if on the 4th step, go back to 1
             else stepCount++;                       // these 2 lines cycle the stepCount from 1 to 4 to know which step the motor is on
+            delay(15);
         }
     }
 
     if (numSteps<0)
     {
-        for (uint8_t i=0; i>numSteps; i--)
+        for (uint16_t i=0; i<(-numSteps); i++)
         {
             motorStep(motorPWM);
             if (stepCount==1) stepCount = 4;        // if on the 1st step, go back to 4
             else stepCount--;                       // these 2 lines cycle the stepCount from 4 to 1
+            delay(15);
         }
     }
 
@@ -125,6 +124,39 @@ void stackRotate(int16_t numSteps)  // Moves the motor a requested number of ste
 
 void motorStep(uint8_t PWM) // Writes to motor pins to perform appropriate step on the stepper motor
 {
+    /*
+    switch(stepCount)
+    {
+        case 1:
+            analogWrite(COIL1_FWD, PWM);
+            analogWrite(COIL1_RVS, 0);
+            analogWrite(COIL2_FWD, PWM);
+            analogWrite(COIL2_RVS, 0);
+            break;
+        
+        case 2:
+            analogWrite(COIL1_FWD, 0);
+            analogWrite(COIL1_RVS, PWM);
+            analogWrite(COIL2_FWD, PWM);
+            analogWrite(COIL2_RVS, 0);
+            break;
+        
+        case 3:
+            analogWrite(COIL1_FWD, 0);
+            analogWrite(COIL1_RVS, PWM);
+            analogWrite(COIL2_FWD, 0);
+            analogWrite(COIL2_RVS, PWM);
+            break;
+        
+        case 4:
+            analogWrite(COIL1_FWD, PWM);
+            analogWrite(COIL1_RVS, 0);
+            analogWrite(COIL2_FWD, 0);
+            analogWrite(COIL2_RVS, PWM);
+            break;
+    }
+    */
+   
     switch(stepCount)
     {
         case 1:
