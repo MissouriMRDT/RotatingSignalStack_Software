4fdc697493343116b1996d91e391867c7c8fe6cd
Grant Brinker <gsbfy2@umsystem.edu>
2023-03-24 21:06:28 -0500

    Added GPS code

--- example/SignalStackSoftware/SignalStackSoftware.h
+++ example/SignalStackSoftware/SignalStackSoftware.h
@@ -27,7 +27,7 @@ IntervalTimer telemetry;
 #define COMPASS_SCL     A5
 
 // GPS serial pins
-#define GPS_RX          0
+#define GPS_RX          0   // these aren't actually needed for the software to work, they are just left here to make it easier to find
 #define GPS_TX          1
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -36,12 +36,21 @@ IntervalTimer telemetry;
 
 
 ////// Variable Definitions ////////////////////////////////////////////////////////////////////////////////////////////////////////
-double signalStackPosition;                 // latitude and longitude
+// Position
+double signalStackPosition[2];              // latitude and longitude
+#define GPS_SERIAL_BAUD 9600
+#define GPS_SERIAL      Serial1
+TinyGPS gps;
+bool newGPSData = false;
+
+// Direction
 float signalStackDirection;                 // Compass angle in degrees
 #define COMPASS_ADDRESS         0x0D        // I2C address for Matek M8Q-5883
 #define COMPASS_DATA_LENGTH     4           // number of bytes required to hold compass data
-uint8_t compassBytes[4];    
-uint8_t compassByte;    
+uint8_t compassBytes[COMPASS_DATA_LENGTH];  // total compass data as an array
+uint8_t compassByte;                        // piece of compass data obtained over I2C
+
+// Movement
 const uint8_t stepDelay = 10;               // milliseconds
 const uint16_t stepsPerRevolution = 3150;   // 360deg / 1.8deg step angle / 15.75 gear reduction
 int16_t stepNumber;                         // number of steps to take, set by rovecomm command
@@ -55,5 +64,6 @@ int16_t stepNumber;                         // number of steps to take, set by r
 void Telemetry();
 void updateCompass();
 void updateGPS();
+void GPSDump(TinyGPS &gps);
 void goToAngle(uint16_t degrees);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- example/SignalStackSoftware/SignalStackSoftware.ino
+++ example/SignalStackSoftware/SignalStackSoftware.ino
@@ -18,7 +18,7 @@ void setup()
     delay(100);
     telemetry.begin(Telemetry, 1500000);
     Serial.println("Started: ");
-    Serial1.begin(115200);
+    GPS_SERIAL.begin(GPS_SERIAL_BAUD);
     
     // motor pins
     pinMode(COIL1_FWD, OUTPUT);
@@ -52,16 +52,44 @@ void loop()
             break;
     }
 
+    // GPS Code
+    unsigned long start = millis();
+    // Every 5 seconds we print an update
+    while (millis() - start < 5000) 
+    {
+        if (GPS_SERIAL.available()) 
+        {
+            char c = GPS_SERIAL.read();
+            //Serial.print(c);  // uncomment to see raw GPS data
+            if (gps.encode(c)) 
+            {
+                newGPSData = true;
+                //break;  // uncomment to print new data immediately!
+            }
+        }
+    }
+    
+    if (newGPSData)
+    {
+        Serial.println("Acquired Data");
+        Serial.println("-------------");
+        gpsDump();
+        updateGPS();
+        Serial.println("-------------");
+        Serial.println();
+    }
+    
     updateCompass();
-    updateGPS();
-    //Telemetry();
+    Telemetry();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Telemetry()
 {
-    RoveComm.write(RC_SIGNALSTACKBOARD_SIGNALSPOSITION_DATA_ID, RC_SIGNALSTACKBOARD_SIGNALSPOSITION_DATA_COUNT, signalStackPosition);
+    if (newGPSData) {
+        RoveComm.write(RC_SIGNALSTACKBOARD_SIGNALSPOSITION_DATA_ID, RC_SIGNALSTACKBOARD_SIGNALSPOSITION_DATA_COUNT, signalStackPosition);
+    }
     RoveComm.write(RC_SIGNALSTACKBOARD_SIGNALSDIRECTION_DATA_ID, RC_SIGNALSTACKBOARD_SIGNALSDIRECTION_DATA_COUNT, signalStackDirection);
 }
 
@@ -87,12 +115,53 @@ void updateCompass()   // Obtains new compass data
 
 void updateGPS()    // Obtains new GPS data
 {
-    signalStackPosition = Serial1.read();
+    static float lat,lon;
+    static unsigned long age;
+    gps.f_get_position(&lat, &lon, &age);
+    signalStackPosition[0] = lat;
+    signalStackPosition[1] = lon; 
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+void GPSDump()
+{
+    static long lat, lon;
+    static unsigned long age, date, time, chars;
+    static int year;
+    byte month, day, hour, minute, second, hundredths;
+    static unsigned short sentences, failed;
 
+    gps.get_position(&lat, &lon, &age);
+    Serial.print("Lat/Long(10^-5 deg): "); Serial.print(lat); Serial.print(", "); Serial.print(lon); 
+    Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+    
+    // On Arduino, GPS characters may be lost during lengthy Serial.print()
+    // On Teensy, Serial prints to USB, which has large output buffering and
+    //   runs very fast, so it's not necessary to worry about missing 4800
+    //   baud GPS characters.
+
+    Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+
+    gps.get_datetime(&date, &time, &age);
+    Serial.print("Date(ddmmyy): "); Serial.print(date); Serial.print(" Time(hhmmsscc): ");
+    Serial.print(time);
+    Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+
+    gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+    Serial.print("Date: "); Serial.print(static_cast<int>(month)); Serial.print("/"); 
+    Serial.print(static_cast<int>(day)); Serial.print("/"); Serial.print(year);
+    Serial.print("  Time: "); Serial.print(static_cast<int>(hour)); Serial.print(":"); 
+    Serial.print(static_cast<int>(minute)); Serial.print(":"); Serial.print(static_cast<int>(second));
+    Serial.print("."); Serial.print(static_cast<int>(hundredths));
+    Serial.print("  Fix age: ");  Serial.print(age); Serial.println("ms.");
+
+    Serial.print("Alt(cm): "); Serial.print(gps.altitude()); Serial.print(" Course(10^-2 deg): ");
+    Serial.print(gps.course()); Serial.print(" Speed(10^-2 knots): "); Serial.println(gps.speed());
+    gps.stats(&chars, &sentences, &failed);
+    Serial.print("Stats: characters: "); Serial.print(chars); Serial.print(" sentences: ");
+    Serial.print(sentences); Serial.print(" failed checksum: "); Serial.println(failed);
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
