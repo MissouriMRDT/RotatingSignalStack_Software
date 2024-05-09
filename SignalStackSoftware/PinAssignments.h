0a314264db624d92ddafd67058b7ef4c179f914a
Grant Brinker <gsbfy2@umsystem.edu>
2024-05-08 21:12:41 -0500

    Update PinAssignments.h

--- SignalStackSoftware/PinAssignments.h
+++ SignalStackSoftware/PinAssignments.h
@@ -1,15 +1,15 @@
 #ifndef PINASSIGNMENTS_H
 #define PINASSIGNMENTS_H
 
-#define JOG_FWD 0 //JOG_A
-#define JOG_RVS 1 //JOG_B
+#define JOG_FWD 33 //JOG_A
+#define JOG_RVS 34 //JOG_B
 
 //Use I2C port Wire1 for compass module
-#define MAG_ENABLE 18
+//#define MAG_ENABLE 18
 
 #define HB_FWD 37
 #define HB_RVS 36
 
-#define COMP_PWR 40
+//#define COMP_PWR 40
 
 #endif //PINASSIGNMENTS_H
