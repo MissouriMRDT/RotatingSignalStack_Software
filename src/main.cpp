bf5df2c0bbf2fcf0526d74c214a18cfa443fa9cc
Meecka <76573720+Meecka@users.noreply.github.com>
2026-03-06 09:21:50 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -57,34 +57,34 @@ public:
 };
 #endif
 
-AK8975 mag(0x0C);
+// AK8975 mag(0x0C);
 
-void setup()
-{
-  Wire.begin();
-  Serial.begin(9600);
-  mag.initialize();
-  if (!mag.testConnection())
-  {
-    Serial.println("AK8975 connection failed!");
-  }
-}
+// void setup()
+// {
+//   Wire.begin();
+//   Serial.begin(9600);
+//   mag.initialize();
+//   if (!mag.testConnection())
+//   {
+//     Serial.println("AK8975 connection failed!");
+//   }
+// }
 
-void loop()
-{
-  int16_t mx, my, mz;
+// void loop()
+// {
+//   int16_t mx, my, mz;
 
-  mag.getHeading(&mx, &my, &mz);
+//   mag.getHeading(&mx, &my, &mz);
 
-  Serial.print("X:");
-  Serial.println(mx);
-  Serial.print("Y:");
-  Serial.println(my);
-  Serial.print("Z:");
-  Serial.println(mz);
+//   Serial.print("X:");
+//   Serial.println(mx);
+//   Serial.print("Y:");
+//   Serial.println(my);
+//   Serial.print("Z:");
+//   Serial.println(mz);
 
-  delay(500);
-}
+//   delay(500);
+// }
 
 // void setup()
 // {
@@ -137,125 +137,125 @@ void loop()
 //   delay(5000);           // wait 5 seconds for next scan
 // }
 
-// #define WATCHDOG_TIMEOUT 1000000
-// IntervalTimer Watchdog;
-// uint8_t watchdogStatus = 0;
-// uint8_t watchdogOverride = 0;
+#define WATCHDOG_TIMEOUT 1000000
+IntervalTimer Watchdog;
+uint8_t watchdogStatus = 0;
+uint8_t watchdogOverride = 0;
 
-// int64_t step_us = 0;
-// bool stepping = false;
-// void estop();
+int64_t step_us = 0;
+bool stepping = false;
+void estop();
 
-// // StepperMC stepper(DIR_PIN, STP_PIN, STEPS);
-// void feedWatchdog() { Watchdog.begin(estop, WATCHDOG_TIMEOUT); }
+// StepperMC stepper(DIR_PIN, STP_PIN, STEPS);
+void feedWatchdog() { Watchdog.begin(estop, WATCHDOG_TIMEOUT); }
 
-// void estop()
-// {
-//   if (!watchdogOverride)
-//   {
-//     analogWrite(STP_PIN, 0);
-//     Serial.println("E-STOP ACTIVATED");
-//   }
-// }
+void estop()
+{
+  if (!watchdogOverride)
+  {
+    analogWrite(STP_PIN, 0);
+    Serial.println("E-STOP ACTIVATED");
+  }
+}
 
-// void setup()
-// {
-//   Serial.begin(9600);
-//   pinMode(LED_BUILTIN, OUTPUT);
-//   pinMode(DIR_PIN, OUTPUT);
-//   digitalWrite(DIR_PIN, HIGH);
-//   pinMode(STP_PIN, OUTPUT);
-//   analogWriteFrequency(STP_PIN, 100);
-// #if ARDUINO_WIZNET_5500_EVB_PICO
-//   analogWriteRange(255);
-// #endif
-//   pinMode(EN_PIN, OUTPUT);
-//   digitalWrite(EN_PIN, LOW);
-//   // RoveComm Initialization
-//   Serial.println("RoveComm Initializing...");
-//   RoveComm.begin(RC_SIGNALSTACKBOARD_IPADDRESS);
-//   Serial.println("Complete");
-// }
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  digitalWrite(DIR_PIN, HIGH);
+  pinMode(STP_PIN, OUTPUT);
+  analogWriteFrequency(STP_PIN, 100);
+#if ARDUINO_WIZNET_5500_EVB_PICO
+  analogWriteRange(255);
+#endif
+  pinMode(EN_PIN, OUTPUT);
+  digitalWrite(EN_PIN, LOW);
+  // RoveComm Initialization
+  Serial.println("RoveComm Initializing...");
+  RoveComm.begin(RC_SIGNALSTACKBOARD_IPADDRESS);
+  Serial.println("Complete");
+}
 
-// int32_t position = 0;
+int32_t position = 0;
 
-// void loop()
-// {
+void loop()
+{
 
-//   RoveComm.read(packet);
+  RoveComm.read(packet);
 
-//   switch (packet.dataId)
-//   {
-//   case RC_SIGNALSTACKBOARD_OPENLOOP_DATA_ID:
-//   {
-//     Serial.print("open loop ");
-//     Serial.println(packet.i16data[0]);
-//     if (packet.i16data[0] < 0)
-//     {
-//       analogWriteFrequency(STP_PIN, -packet.i16data[0]);
-//       analogWrite(STP_PIN, 128);
-//       digitalWrite(DIR_PIN, HIGH);
-//       // int16_t reverseSpeed = -packet.i16data[0];
+  switch (packet.dataId)
+  {
+  case RC_SIGNALSTACKBOARD_OPENLOOP_DATA_ID:
+  {
+    Serial.print("open loop ");
+    Serial.println(packet.i16data[0]);
+    if (packet.i16data[0] < 0)
+    {
+      analogWriteFrequency(STP_PIN, -packet.i16data[0]);
+      analogWrite(STP_PIN, 128);
+      digitalWrite(DIR_PIN, HIGH);
+      // int16_t reverseSpeed = -packet.i16data[0];
 
-//       // stepper.reverseDir(true);
-//       // stepper.setSpeed(-5000, 0);
-//       // stepper.setIncrementsRelative(movingIncrements);
-//       // stepper.moveTarget();
-//     }
-//     else if (packet.i16data[0] > 0)
-//     {
-//       analogWriteFrequency(STP_PIN, packet.i16data[0]);
-//       analogWrite(STP_PIN, 128);
-//       digitalWrite(DIR_PIN, LOW);
-//       //  stepper.reverseDir(false);
-//       // stepper.setSpeed(5000, 0);
-//       // stepper.setIncrementsRelative(movingIncrements);
-//       // stepper.moveTarget();
-//     }
-//     else
-//     {
-//       analogWrite(STP_PIN, 0);
-//       // stepper.setSpeed(0, 0);
-//     }
-//     feedWatchdog();
-//     break;
-//   }
-//   case RC_SIGNALSTACKBOARD_SETANGLETARGET_DATA_ID:
-//   {
+      // stepper.reverseDir(true);
+      // stepper.setSpeed(-5000, 0);
+      // stepper.setIncrementsRelative(movingIncrements);
+      // stepper.moveTarget();
+    }
+    else if (packet.i16data[0] > 0)
+    {
+      analogWriteFrequency(STP_PIN, packet.i16data[0]);
+      analogWrite(STP_PIN, 128);
+      digitalWrite(DIR_PIN, LOW);
+      //  stepper.reverseDir(false);
+      // stepper.setSpeed(5000, 0);
+      // stepper.setIncrementsRelative(movingIncrements);
+      // stepper.moveTarget();
+    }
+    else
+    {
+      analogWrite(STP_PIN, 0);
+      // stepper.setSpeed(0, 0);
+    }
+    feedWatchdog();
+    break;
+  }
+  case RC_SIGNALSTACKBOARD_SETANGLETARGET_DATA_ID:
+  {
 
-//     float degreesToMove = packet.fdata[0];
-//     float stepsPerDegree = STEPS / 360.0;
-//     int32_t steps = (int32_t)roundf(degreesToMove * stepsPerDegree);
-//     // analogWriteFrequency(STP_PIN, map(packet.i16data[0], 0, 1000, 0, 1000));
-//     // analogWrite(STP_PIN, steps);
-//     digitalWrite(DIR_PIN, HIGH);
-//     for (int i = 0; i < steps; i++)
-//     {
-//       digitalWrite(DIR_PIN, HIGH);
-//       delay(1);
-//       digitalWrite(DIR_PIN, LOW);
-//       delay(1);
-//     }
-//     // stepper.setIncrements(steps);
-//     //  stepper.setIncrementsRelative(steps);
-//     feedWatchdog();
-//     break;
-//   }
-//   // case RC_SIGNALSTACKBOARD_SETGPSTARGET_DATA_ID: {
+    float degreesToMove = packet.fdata[0];
+    float stepsPerDegree = STEPS / 360.0;
+    int32_t steps = (int32_t)roundf(degreesToMove * stepsPerDegree);
+    // analogWriteFrequency(STP_PIN, map(packet.i16data[0], 0, 1000, 0, 1000));
+    // analogWrite(STP_PIN, steps);
+    digitalWrite(DIR_PIN, HIGH);
+    for (int i = 0; i < steps; i++)
+    {
+      digitalWrite(DIR_PIN, HIGH);
+      delay(1);
+      digitalWrite(DIR_PIN, LOW);
+      delay(1);
+    }
+    // stepper.setIncrements(steps);
+    //  stepper.setIncrementsRelative(steps);
+    feedWatchdog();
+    break;
+  }
+  // case RC_SIGNALSTACKBOARD_SETGPSTARGET_DATA_ID: {
 
-//   //   break;
-//   // }
-//   case RC_SIGNALSTACKBOARD_WATCHDOGOVERRIDE_DATA_ID:
-//   {
-//     watchdogOverride = packet.i8data[0];
-//     break;
-//   }
-//     // case RC_SIGNALSTACKBOARD_COMPASSANGLE_DATA_ID: {
+  //   break;
+  // }
+  case RC_SIGNALSTACKBOARD_WATCHDOGOVERRIDE_DATA_ID:
+  {
+    watchdogOverride = packet.i8data[0];
+    break;
+  }
+    // case RC_SIGNALSTACKBOARD_COMPASSANGLE_DATA_ID: {
 
-//     //   break;
-//     // }
-//   }
-// #if ARDUINO_WIZNET_5500_EVB_PICO
-//   Watchdog.update();
-// #endif
-// }
+    //   break;
+    // }
+  }
+#if ARDUINO_WIZNET_5500_EVB_PICO
+  Watchdog.update();
+#endif
+}
