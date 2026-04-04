3b306ecf4fa17a3d12cae618d9af623dfe21d500
Meecka <76573720+Meecka@users.noreply.github.com>
2026-04-03 23:02:32 -0500

    compass software

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <RoveComm.h>
 #include <Wire.h>
 #include "AK8975.h"
+#include <cmath>
 
 // the number of steps on your motor
 #define STEPS 200
@@ -19,6 +20,12 @@
 #define COMPASS_SDA 6
 #define COMPASS_SCL 7
 #endif
+
+#define DEGTORAD(x) (x * (180 / M_PI))
+
+AK8975 mag(0x0C);
+int16_t mx, my, mz;
+float heading;
 
 RoveCommPacket packet;
 RoveCommEthernet RoveComm;
@@ -55,87 +62,16 @@ public:
     }
   }
 };
+
+template <typename... Args>
+void Serial_printf(const char *fmt, Args... args)
+{
+  char buffer[1024];
+  snprintf(buffer, 1024, fmt, args...);
+  Serial.print(buffer);
+}
+
 #endif
-
-// AK8975 mag(0x0C);
-
-// void setup()
-// {
-//   Wire.begin();
-//   Serial.begin(9600);
-//   mag.initialize();
-//   if (!mag.testConnection())
-//   {
-//     Serial.println("AK8975 connection failed!");
-//   }
-// }
-
-// void loop()
-// {
-//   int16_t mx, my, mz;
-
-//   mag.getHeading(&mx, &my, &mz);
-
-//   Serial.print("X:");
-//   Serial.println(mx);
-//   Serial.print("Y:");
-//   Serial.println(my);
-//   Serial.print("Z:");
-//   Serial.println(mz);
-
-//   delay(500);
-// }
-
-// void setup()
-// {
-//   Wire.begin();
-
-//   Serial.begin(9600);
-//   while (!Serial);             // Leonardo: wait for serial monitor
-//   Serial.println("\nI2C Scanner");
-// }
-
-// void loop()
-// {
-//   byte error, address;
-//   int nDevices;
-
-//   Serial.println("Scanning...");
-
-//   nDevices = 0;
-//   for(address = 1; address < 127; address++ )
-//   {
-//     // The i2c_scanner uses the return value of
-//     // the Write.endTransmisstion to see if
-//     // a device did acknowledge to the address.
-//     Wire.beginTransmission(address);
-//     error = Wire.endTransmission();
-
-//     if (error == 0)
-//     {
-//       Serial.print("I2C device found at address 0x");
-//       if (address<16)
-//         Serial.print("0");
-//       Serial.print(address,HEX);
-//       Serial.println("  !");
-
-//       nDevices++;
-//     }
-//     else if (error==4)
-//     {
-//       Serial.print("Unknown error at address 0x");
-//       if (address<16)
-//         Serial.print("0");
-//       Serial.println(address,HEX);
-//     }
-//   }
-//   if (nDevices == 0)
-//     Serial.println("No I2C devices found\n");
-//   else
-//     Serial.println("done\n");
-
-//   delay(5000);           // wait 5 seconds for next scan
-// }
 
 #define WATCHDOG_TIMEOUT 1000000
 IntervalTimer Watchdog;
@@ -145,6 +81,12 @@ uint8_t watchdogOverride = 0;
 int64_t step_us = 0;
 bool stepping = false;
 void estop();
+
+IntervalTimer stepTimer;
+
+volatile long stepsRemaining = 0;
+const int stepPin = STP_PIN;
+const int dirPin = DIR_PIN;
 
 // StepperMC stepper(DIR_PIN, STP_PIN, STEPS);
 void feedWatchdog() { Watchdog.begin(estop, WATCHDOG_TIMEOUT); }
@@ -160,11 +102,18 @@ void estop()
 
 void setup()
 {
+  Wire.begin();
   Serial.begin(9600);
+  Serial.println("Initializing I2C devices...");
+  mag.initialize();
+  Serial.println("Testing device connections...");
+  Serial.println(mag.testConnection() ? "AK8975 connection successful" : "AK8975 connection failed");
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
   digitalWrite(DIR_PIN, HIGH);
   pinMode(STP_PIN, OUTPUT);
+
   analogWriteFrequency(STP_PIN, 100);
 #if ARDUINO_WIZNET_5500_EVB_PICO
   analogWriteRange(255);
@@ -178,14 +127,20 @@ void setup()
 }
 
 int32_t position = 0;
+float_t targetAngle = 0;
+double_t roverLat = 0;
+double_t roverLon = 0;
+double_t baseLat = 0;
+double_t baseLon = 0;
+#define M_PI 3.14159265358979323846
+bool closedLoopActive = false;
 
 void loop()
 {
-
   RoveComm.read(packet);
-
   switch (packet.dataId)
   {
+  // speed
   case RC_SIGNALSTACKBOARD_OPENLOOP_DATA_ID:
   {
     Serial.print("open loop ");
@@ -195,65 +150,116 @@ void loop()
       analogWriteFrequency(STP_PIN, -packet.i16data[0]);
       analogWrite(STP_PIN, 128);
       digitalWrite(DIR_PIN, HIGH);
-      // int16_t reverseSpeed = -packet.i16data[0];
-
-      // stepper.reverseDir(true);
-      // stepper.setSpeed(-5000, 0);
-      // stepper.setIncrementsRelative(movingIncrements);
-      // stepper.moveTarget();
     }
     else if (packet.i16data[0] > 0)
     {
       analogWriteFrequency(STP_PIN, packet.i16data[0]);
       analogWrite(STP_PIN, 128);
       digitalWrite(DIR_PIN, LOW);
-      //  stepper.reverseDir(false);
-      // stepper.setSpeed(5000, 0);
-      // stepper.setIncrementsRelative(movingIncrements);
-      // stepper.moveTarget();
     }
     else
     {
       analogWrite(STP_PIN, 0);
-      // stepper.setSpeed(0, 0);
     }
+    closedLoopActive = false;
     feedWatchdog();
     break;
   }
   case RC_SIGNALSTACKBOARD_SETANGLETARGET_DATA_ID:
   {
+    Serial.print("received: ");
+    Serial.println(packet.fdata[0]);
+    float targetAngle = packet.fdata[0]; // Assuming angle is sent as a float
+    static float currentAngle = 0;
 
-    float degreesToMove = packet.fdata[0];
-    float stepsPerDegree = STEPS / 360.0;
-    int32_t steps = (int32_t)roundf(degreesToMove * stepsPerDegree);
-    // analogWriteFrequency(STP_PIN, map(packet.i16data[0], 0, 1000, 0, 1000));
-    // analogWrite(STP_PIN, steps);
-    digitalWrite(DIR_PIN, HIGH);
-    for (int i = 0; i < steps; i++)
+    // 1. Calculate the delta (change) needed
+    float angleToMove = targetAngle - currentAngle;
+
+    // 2. Convert angle to steps (Example: 1.8 degrees per step)
+    // Formula: Steps = Angle / 1.8
+    long stepsToMove = abs(angleToMove) / 1.40625;
+
+    // 3. Set Direction
+    digitalWrite(DIR_PIN, (angleToMove > 0) ? HIGH : LOW);
+
+    // 4. Step execution (Blocking approach for simplicity)
+    // Note: For smoother movement, use the AccelStepper library
+    for (long i = 0; i < stepsToMove; i++)
     {
-      digitalWrite(DIR_PIN, HIGH);
-      delay(1);
-      digitalWrite(DIR_PIN, LOW);
-      delay(1);
+      Serial.print("moving");
+      digitalWrite(STP_PIN, HIGH);
+      delayMicroseconds(2000); // Controls speed
+      digitalWrite(STP_PIN, LOW);
+      delayMicroseconds(2000);
     }
-    // stepper.setIncrements(steps);
-    //  stepper.setIncrementsRelative(steps);
+
+    // 5. Update state
+    currentAngle = targetAngle;
+    closedLoopActive = true;
     feedWatchdog();
     break;
   }
-  // case RC_SIGNALSTACKBOARD_SETGPSTARGET_DATA_ID: {
-
+  // heading
+  // case RC_SIGNALSTACKBOARD_SETANGLETARGET_DATA_ID:
+  // {
+  //   targetAngle = packet.fdata[0];
+  //   feedWatchdog();
   //   break;
   // }
+  // gps
+  case RC_SIGNALSTACKBOARD_SETGPSTARGET_DATA_ID:
+  {
+    // MY LOCATION: 37.951664, -91.777234
+    // PRICE: 37.95388110046358, -91.751811233115
+
+    // ROVER/CHANGING GPS: 37.95180969121836, -91.77626487233822
+    // DELC/SIGNAL GPS: 37.951974771528725, -91.7772764007855
+    roverLat = packet.ddata[0];
+    Serial_printf("roverLat: %.10f\n", packet.ddata[0]);
+    roverLon = packet.ddata[1];
+    Serial_printf("roverLon: %.10f\n", packet.ddata[1]);
+    baseLat = packet.ddata[2];
+    Serial_printf("baseLat: %.10f\n", packet.ddata[2]);
+    baseLon = packet.ddata[3];
+    Serial_printf("baseLon: %.10f\n", packet.ddata[3]);
+
+    mag.getHeading(&mx, &my, &mz);
+
+    // display tab-separated magnetometer x/y/z values
+    Serial.print("mag:\t");
+    Serial.print(mx);
+    Serial.print("\t");
+    Serial.print(my);
+    Serial.print("\t");
+    Serial.print(mz);
+    Serial.print("\t\t");
+
+    // angle from signalstack relative to north
+    float azimuth = DEGTORAD(atan2(mx, my));
+    if (azimuth < 0)
+    {
+      azimuth += 360.0;
+    }
+
+    Serial_printf("angle from signalstack relative to north: %.10f\n", azimuth);
+
+    // angle from signalstack->rover vector relative to north
+    float theta = DEGTORAD(atan2((roverLon - baseLon), (roverLat - baseLat)));
+
+    Serial_printf("angle from signalstack->rover vector relative to north: %.10f\n", theta);
+
+    float differenceAngle = azimuth - theta;
+
+    //
+
+    feedWatchdog();
+    break;
+  }
   case RC_SIGNALSTACKBOARD_WATCHDOGOVERRIDE_DATA_ID:
   {
     watchdogOverride = packet.i8data[0];
     break;
   }
-    // case RC_SIGNALSTACKBOARD_COMPASSANGLE_DATA_ID: {
-
-    //   break;
-    // }
   }
 #if ARDUINO_WIZNET_5500_EVB_PICO
   Watchdog.update();
